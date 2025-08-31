07ac5d3b0a3aa26fce9085202e1b483a7a5fba3c
tillwenke <till.wenke@gmx.net>
2025-08-31 20:33:24 +0200

    feat: posting past hitchhiking rides

---
+++ nostr/README.md
@@ -0,0 +1,56 @@
+
+
+To mitigate data duplication and data being injected into "our" ecosystem from outside actors we collect people (identified by their npub) and the organization/ application (`source`) which they publish hitchhiking rides for here. Thus in case the data gets compromised filtering by the following should yield data from trustworthy sources again.
+
+| npub         | sources            |                                           
+|---------------|------------------------|
+| Till | liftershalte.info, hitchmap.com, hitchwiki.org |
+
+# Examples
+
+`publish_past_rides.py` ... shows an example of how to take a present dataset of hitchhiking rides, transform it into the defined standard and to post it to Nostr so that others can access it. You might have to adapt a few things for your own dataset. You might have to adapt a few more things to do this in another language than python.
+
+`publish_ride.py` ... shows an example of how to take a ride that was just recorded e.g. on a hitchhiking application with opionionated fields that were collected by the application, transform it into the defined standard and to post it to Nostr so that others can access it.
+
+To get started with those script you need to set up the python evironment as follows:
+
+```shell
+cp example.env .env
+# set env vars in .env
+# using python 3.12
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+You and other people have published their hitchhiking rides using the Nostr protocol, thus you can access this common collection of rides.
+
+`fetch_hitchhiking_events/` ... shows how this can be done.
+
+# Nostr Keys
+
+| Nostr Key           | Value Type             | Required? | Purpose & Notes                                           
+|---------------|------------------------|-----------|------------------------------------------------------------|
+| `kind`           | Int    | Yes  | `36820` |       
+| `created_at`           | Int   | Yes   | unix timestamp in seconds     
+| `content`           | String   | Yes   | A json object as string using the tags from `STANDARD.md`. At least the `required` tags have to be present and not-null. For tags where no information is given for a ride simply leave the tag out (do not use null values). |
+| `pubkey`           | String    | Yes   | 32-bytes lowercase hex-encoded public key of the event creator. See [NIP-01](https://github.com/nostr-protocol/nips/blob/master/01.md). |              
+| `id`           | String    | Yes   | 32-bytes lowercase hex-encoded sha256 of the serialized event data. See [NIP-01](https://github.com/nostr-protocol/nips/blob/master/01.md). |              
+| `sig`           | String    | Yes   | 64-bytes lowercase hex of the signature of the sha256 hash of the serialized event data, which is the same as the "id" field. See [NIP-01](https://github.com/nostr-protocol/nips/blob/master/01.md). |              
+
+
+
+# Tag Structure
+Use the following Nostr-specific tags that make interacting with the Nostr events easier.
+
+| Tag           | Value Type             | Required? | Purpose & Notes                                            | NIP-99 Standard? |
+|---------------|------------------------|-----------|------------------------------------------------------------|-----------------|
+| `d`           | String (UUID recommended)     | Yes   | Unique identifier for the specific ride. Prevents duplicates, enables updates/deletion. By posting a new event with the same d tag its content can be changed - the older version of the event may be discarded by relays. Each application posting hitchhiking events can come up with their own schema, it is recommended to use "`source`-`version 4 UUID`". | Yes             |
+| `g`           | String (Geohash Prefix)| Yes       | **Origin** geohash (at least of length 10) for area filtering. Has to be equivalent to the loaction of the first `stop` object in the `stops` field in `content` (the starting location). | Yes    
+| `published_at`           | String (UNIX timestamp) | Yes       | The timestamp (in unix seconds – converted to string) of the first version of this ride that was published as a Nostr event. If a `source` application directly posts events to Nostr this is equivalent to the time in `submission_time` field in `content`. | Yes           |
+| `expiration`           | int| No       | If you are still testing and playing around with Nostr events set this to `0`. Otherwise this tag is not needed. | -             |
+
+
+# Example Nostr event
+
+See [nostr.json](nostr.json).
---
+++ nostr/publish_past_rides.py
@@ -0,0 +1,158 @@
+"""
+An example of how to take a present dataset of hitchhiking rides,
+transform it into the defined standard and to post it to Nostr so that others can access it.
+"""
+import sys
+import os
+import wget
+
+import sqlite3
+import pandas as pd
+from tqdm import tqdm
+
+sys.path.append("../python")
+
+from utils.post_hitchhiking_ride_to_nostr import HitchhikingDataStandardToNostrPoster
+from hitchhiking_data_standard_pydantic_model import Hitchhiker, HitchhikingRecord, Location, Signal, Stop
+
+### Load your dataset of past hitchhiking rides
+
+url = 'https://hitchmap.com/dump.sqlite'
+filename = 'dump.sqlite'
+if os.path.exists(filename):
+        os.remove(filename)
+filename = wget.download(url)
+fn = 'dump.sqlite'
+points = pd.read_sql('select * from points where not banned', sqlite3.connect(fn))
+points["datetime"] = points["datetime"].astype("datetime64[ns]")
+
+### Clean your dataset from issues that your are already aware of
+### This prevents that others have to do it when fetching these rides
+
+points.loc[points["datetime"] < "2000-01-01", "datetime"] = None
+
+points["ride_datetime"] = points["ride_datetime"].replace("0224-10-31T21:30", None)
+points["ride_datetime"] = points["ride_datetime"].replace("0025-03-07T08:00", None)
+points["ride_datetime"] = points["ride_datetime"].replace("1014-11-04T14:30", None)
+points["ride_datetime"] = points["ride_datetime"].replace("0202-04-03T18:50", None)
+
+points["ride_datetime"] = points["ride_datetime"].astype("datetime64[ns]")
+
+### This is a unique step that is performed here where we post all rides from 20 years of rides
+
+# assume that during the lifershalte time the timestamps where not always set
+# thus attribute this part of the dataset to the lifershalte
+no_date = points[points["datetime"].isna()]
+with_date = points[~points["datetime"].isna()]
+
+lift = pd.concat([no_date, with_date[with_date["datetime"] < "2010-08-11"]])
+
+wiki = with_date[(with_date["datetime"] >= "2010-08-11") & (with_date["datetime"] < "2022-10-13")]
+
+hitchmap = with_date[with_date["datetime"] >= "2022-10-13"]
+
+### Define functions that create the objects demanded by this standard from the possibly unique data that is used in your dataset
+
+def map_signal(signal: str) -> Signal:
+    if not signal:
+        return None
+
+    if signal == "sign":
+        return Signal(
+            methods=["sign"],
+        )
+    elif signal == "thumb":
+        return Signal(
+            methods=["thumb"],
+        )
+    elif signal == "ask":
+        return Signal(
+            methods=["asking"],
+        )
+    elif signal == "ask-sign":
+        return Signal(
+            methods=["asking", "sign"],
+        )
+    else:
+        return None
+
+
+### Define one function that takes single rides from your dataset and builds objects that follow this standard from them
+### Again, here the function is a bit special because we are dealing with multiple datasets actually
+
+def create_record_from_row(row: pd.Series, source: str, license: str, rating_formula= lambda x: x) -> HitchhikingRecord:
+    stops = [
+        Stop(
+            location=Location(latitude=row["lat"], longitude=row["lon"], is_exact=True),
+            arrival_time=row["ride_datetime"].strftime("%Y-%m-%dT%H:%M:%S") if pd.notna(row["ride_datetime"]) else None,
+            departure_time=(row["ride_datetime"] + pd.to_timedelta(row["wait"], unit="m")).strftime(
+                "%Y-%m-%dT%H:%M:%S"
+            )
+            if pd.notna(row["ride_datetime"]) and pd.notna(row["wait"])
+            else None,
+            waiting_duration=f"{int(row['wait'])}M" if pd.notna(row["wait"]) else None,
+        ),
+    ]
+    if pd.notna(row["dest_lat"]) and pd.notna(row["dest_lon"]):
+        stops.append(Stop(location=Location(latitude=row["dest_lat"], longitude=row["dest_lon"], is_exact=False)))
+
+    record = HitchhikingRecord(
+        version="0.0.0",
+        stops=stops,
+        rating=rating_formula(row["rating"]),
+        hitchhikers=[
+            Hitchhiker(
+                nickname=row["nickname"] if pd.notna(row["nickname"]) else "Anonymous"
+            )
+        ],
+        comment=row["comment"],
+        signals=[map_signal(row["signal"])] if row["signal"] else None,
+        occupants=None,
+        mode_of_transportation=None,
+        ride=None,
+        declined_rides=None,
+        source=source,
+        license=license,
+        submission_time=row["datetime"].strftime("%Y-%m-%dT%H:%M:%S") if pd.notna(row["datetime"]) else None,
+    )
+
+    return record
+
+### Collect the records that are now in the desired format
+
+hitchhiking_records = []
+
+for _, row in tqdm(lift.iterrows(), total=len(lift)):
+    hitchhiking_records.append(
+        create_record_from_row(
+            row,
+            source="liftershalte.info",
+            license="cc-by-sa-4.0",
+        )
+    )
+
+for _, row in tqdm(wiki.iterrows(), total=len(wiki)):
+    hitchhiking_records.append(
+        create_record_from_row(
+            row,
+            source="hitchwiki.org",
+            license="cc-by-sa-4.0",
+        )
+    )
+
+for _, row in tqdm(hitchmap.iterrows(), total=len(hitchmap)):
+    hitchhiking_records.append(
+        create_record_from_row(
+            row,
+            source="hitchmap.com",
+            license="odbl",
+        )
+    )
+
+### Post your records to the Nostr protocol to publish them
+
+poster = HitchhikingDataStandardToNostrPoster()
+for record in hitchhiking_records[-10:]:
+    poster.post(ride_record=record)
+
+poster.close()
